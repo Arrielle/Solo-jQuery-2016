79e02a95c49c6405d68caeee111c6f6a939ac645
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-20 13:22:16 -0600

    adding colored cubes on button click

--- client.js
+++ client.js
@@ -1,11 +1,19 @@
 $(document).ready (function (){
-console.log('docready is working');
+
+// $('#color-block-container').append('<div class = "color-cube"></div>');
 
 $('button').on('click', function(){
-  console.log('a button was clicked');
 
   var color = $(this).data("color");
   console.log(color);
+
+  $('#color-block-container').append('<div class = "color-cube ' + color + '"></div>');
+
+// $('.container').append('<div  id = "color-cube" class ="' + color + '"></div>');
+
 });//ends on click function (button)
 
 });//ends doc ready
+
+
+//add colored divs when a button is pressed.
