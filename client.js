073e8182034d63b47f719608c6a3c39e1d0611c7
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-20 13:40:44 -0600

    finished project. Counters now work

--- client.js
+++ client.js
@@ -3,17 +3,19 @@ $(document).ready (function (){
 // $('#color-block-container').append('<div class = "color-cube"></div>');
 
 $('button').on('click', function(){
+  var $color = $(this).data("color");
+  console.log($color);
+  $('#color-block-container').append('<div class = "color-cube ' + $color + '"></div>');
 
-  var color = $(this).data("color");
-  console.log(color);
-
-  $('#color-block-container').append('<div class = "color-cube ' + color + '"></div>');
-
-// $('.container').append('<div  id = "color-cube" class ="' + color + '"></div>');
-
+  numColorBlocks = $('#' + $color).text();
+  console.log(numColorBlocks);
+  $('#' + $color).text(parseFloat(numColorBlocks) + 1);
 });//ends on click function (button)
-
 });//ends doc ready
 
 
 //add colored divs when a button is pressed.
+
+// when a button is pressed, the counter relative to that div goes up.
+//  span ID = red
+//for every time the button is clicked, number in text box it incremented.
