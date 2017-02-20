9e83c183303ab17022fcb15247b3fe9a1dec36ef
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-20 13:42:11 -0600

    changing html and css back to original

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ $(document).ready (function (){
 $('button').on('click', function(){
   var $color = $(this).data("color");
   console.log($color);
-  $('#color-block-container').append('<div class = "color-cube ' + $color + '"></div>');
+  $('.container').append('<div class = "color-cube ' + $color + '"></div>');
 
   numColorBlocks = $('#' + $color).text();
   console.log(numColorBlocks);
