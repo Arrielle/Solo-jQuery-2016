d3d4158eb3ff60b3cc4a5cb59ae352a25994731f
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-20 14:41:03 -0600

    cleaning out code

--- client.js
+++ client.js
@@ -1,21 +1,9 @@
 $(document).ready (function (){
 
-// $('#color-block-container').append('<div class = "color-cube"></div>');
-
 $('button').on('click', function(){
   var $color = $(this).data("color");
-  console.log($color);
   $('.container').append('<div class = "color-cube ' + $color + '"></div>');
-
   numColorBlocks = $('#' + $color).text();
-  console.log(numColorBlocks);
   $('#' + $color).text(parseFloat(numColorBlocks) + 1);
 });//ends on click function (button)
 });//ends doc ready
-
-
-//add colored divs when a button is pressed.
-
-// when a button is pressed, the counter relative to that div goes up.
-//  span ID = red
-//for every time the button is clicked, number in text box it incremented.
